abde7ac6aa2c57ff6c10bb7b55c954c924f16849
QuintonEL <quintonlaborde@gmail.com>
2021-09-09 14:50:55 -0600

    implemented 5 day forecast

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ const api = {
 
 function App() {
   const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState({});
+  const [forecast, setForecast] = useState({});
 
   const search = evt => {
     if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}forecast?q=${query}&units=metric&appid=${api.key}`)
       .then(res => res.json())
       .then(result => {
-        setWeather(result);
+        setForecast(result);
         setQuery('');
         console.log(result);
       });
@@ -40,24 +40,68 @@ function App() {
           <input
             type="text"
             className="search-bar"
-            placeHolder="Search..."
+            placeHolder="Enter a city!"
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           >
           </input>
         </div>
-        {(typeof weather.main != "undefined") ? (
+        {(typeof forecast.list != "undefined") ? ( //check if a search query has been done
           <div>
             <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="location">{forecast.city.name}, {forecast.city.country}</div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
               <div className="temp">
-                {Math.round(weather.main.temp)}°C
+                {Math.round(forecast.list[0].main.temp)}°C
+                <div className="weather">{forecast.list[0].weather[0].main}</div>
               </div>
-              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+            <div className="header">
+              Daily
+            </div>
+            <div className="forecast">
+              <div className="col">
+                <div className="small-date">{(forecast.list[1].dt_txt).slice(0, -9)}</div>
+                <div className="temp">
+                  {Math.round(forecast.list[1].main.temp)}°C
+                </div>
+                <div className="weather">{forecast.list[1].weather[0].main}</div>
+              </div>
+
+              <div className="col">
+                <div className="small-date">{(forecast.list[9].dt_txt).slice(0, -9)}</div>
+                  <div className="temp">
+                    {Math.round(forecast.list[9].main.temp)}°C
+                  </div>
+                <div className="weather">{forecast.list[9].weather[0].main}</div>
+              </div> 
+
+              <div className="col">
+                <div className="small-date">{(forecast.list[17].dt_txt).slice(0, -9)}</div>
+                  <div className="temp">
+                    {Math.round(forecast.list[17].main.temp)}°C
+                  </div>
+                <div className="weather">{forecast.list[17].weather[0].main}</div>
+              </div>
+
+              <div className="col">
+                <div className="small-date">{(forecast.list[25].dt_txt).slice(0, -9)}</div>
+                  <div className="temp">
+                    {Math.round(forecast.list[25].main.temp)}°C
+                  </div>
+                <div className="weather">{forecast.list[25].weather[0].main}</div>
+              </div>
+
+              <div className="col">
+                <div className="small-date">{(forecast.list[33].dt_txt).slice(0, -9)}</div>
+                  <div className="temp">
+                    {Math.round(forecast.list[33].main.temp)}°C
+                  </div>
+                <div className="weather">{forecast.list[33].weather[0].main}</div>
+              </div>
             </div>
           </div>
         ) : ('')}
