b7b0b020283117dc8acc2da7f2c0442c75b137a2
QuintonEL <quintonlaborde@gmail.com>
2021-09-09 10:38:03 -0600

    added search bar and base styling for the page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React from "react";
+
+const api = {
+  key: "8e4863d596f6b5dbac14fc594c9b1ead",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeHolder="Search..."
+          >
+          </input>
+        </div>
+      </main>
     </div>
   );
 }
